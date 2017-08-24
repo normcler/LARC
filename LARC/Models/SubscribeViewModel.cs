5977f719f070b823420b889761eb7907bd9397a3
Norm Clerman <norm.clerman@gmail.com>
2017-08-24 11:00:55 -0700

    Added SubscribeViewModel

---
+++ LARC/Models/SubscribeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LARC.Models
+{
+    public class SubscribeViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "e-mail address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password",
+            ErrorMessage = "Confirm password doesn't match, Type again !")]
+        public string ConfirmPassword { get; set; }
+    }
+}
