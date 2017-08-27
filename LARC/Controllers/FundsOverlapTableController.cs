31e20bece8aa3bd860ab36680e0aff566dedeca2
Norm Clerman <norm.clerman@gmail.com>
2017-08-26 19:08:28 -0700

    Basic FundsOverlapTable working, with preliminary styling.

--- LARC/Controllers/FundsOverlapTableController.cs
+++ LARC/Controllers/FundsOverlapTableController.cs
@@ -7,76 +7,73 @@ using LARC.Models;
 
 namespace LARC.Controllers
 {
-    public class FundsOverlapTableController : Controller
+  public class FundsOverlapTableController : Controller
+  {
+    private static List<HoldingOverlap> testOverlapList
+        = new List<HoldingOverlap>();
+    private static FundsOverlapTable testTable = new FundsOverlapTable();
+
+    [NonAction]
+    public static void InitializeTestTable()
     {
-        private static List<HoldingOverlap> testOverlapList
-            = new List<HoldingOverlap>();
-        private static FundsOverlapTable testTable = new FundsOverlapTable();
+      testOverlapList.
+          Add(new HoldingOverlap
+          {
+            HoldingTicker = "BMRN",
+            HoldingName = "Biomarin Pharmaceutical Inc",
+            Overlap = 0.595M
+          });
+      testOverlapList.
+          Add(new HoldingOverlap
+          {
+            HoldingTicker = "ALGN",
+            HoldingName = "Align Technology Inc",
+            Overlap = 0.290M
+          });
+      testOverlapList.
+          Add(new HoldingOverlap
+          {
+            HoldingTicker = "TFX",
+            HoldingName = "Teleflex Inc",
+            Overlap = 0.605M
+          });
+      testOverlapList.
+          Add(new HoldingOverlap
+          {
+            HoldingTicker = "ALKS",
+            HoldingName = "Alkermes PLC",
+            Overlap = 0.605M
+          });
+      testOverlapList.
+          Add(new HoldingOverlap
+          {
+            HoldingTicker = "WCG",
+            HoldingName = "WellCare Health Plans Inc",
+            Overlap = 0.333M
+          });
+      testOverlapList.
+          Add(new HoldingOverlap
+          {
+            HoldingTicker = "WST",
+            HoldingName = "West Pharmaceutical Services Inc",
+            Overlap = 0.435M
+          });
 
-
-
-
-        [NonAction]
-        public static void InitializeTestTable()
-        {
-            testOverlapList.
-                Add(new HoldingOverlap
-                {
-                    HoldingTicker = "BMRN",
-                    HoldingName = "Biomarin Pharmaceutical Inc",
-                    Overlap = 0.595M
-                });
-            testOverlapList.
-                Add(new HoldingOverlap
-                {
-                    HoldingTicker = "ALGN",
-                    HoldingName = "Align Technology Inc",
-                    Overlap = 0.290M
-                });
-            testOverlapList.
-                Add(new HoldingOverlap
-                {
-                    HoldingTicker = "TFX",
-                    HoldingName = "Teleflex Inc",
-                    Overlap = 0.605M
-                });
-            testOverlapList.
-                Add(new HoldingOverlap
-                {
-                    HoldingTicker = "ALKS",
-                    HoldingName = "Alkermes PLC",
-                    Overlap = 0.605M
-                });
-            testOverlapList.
-                Add(new HoldingOverlap
-                {
-                    HoldingTicker = "WCG",
-                    HoldingName = "WellCare Health Plans Inc",
-                    Overlap = 0.333M
-                });
-            testOverlapList.
-                Add(new HoldingOverlap
-                {
-                    HoldingTicker = "WST",
-                    HoldingName = "West Pharmaceutical Services Inc",
-                    Overlap = 0.435M
-                });
-
-            testTable.FundSymbol_1 ="FSEVX";
-            testTable.FundSymbol_2 = "PRHSX";
-            testTable.OverlapList = testOverlapList;
-        }
-        // GET: FundsOverlapTable
-        public ActionResult Index()
-        {
-            //  By passing an argument to the view method, we are "binding" the
-            //  testOverlapList object to the view.  In this way, the
-            //  testOverlapListobject will be accessible in the cshtml.
-
-            //  This is the "Model" refered to in the corresponding view.
-            return View(testTable);
-        }
+      testTable.FundSymbol_1 = "FSEVX";
+      testTable.FundSymbol_2 = "PRHSX";
+      testTable.OverlapList = testOverlapList;
     }
+    // GET: FundsOverlapTable
+    public ActionResult Index()
+    {
+      //  By passing an argument to the view method, we are "binding" the
+      //  testOverlapList object to the view.  In this way, the
+      //  testOverlapListobject will be accessible in the cshtml.
 
-        
+      //  This is the "Model" refered to in the corresponding view.
+      return View(testTable);
     }
+  }
+
+
+}
--- LARC/Models/HoldingOverlap.cs
+++ LARC/Models/HoldingOverlap.cs
@@ -6,30 +6,30 @@ using System.Threading.Tasks;
 
 namespace LARC.Models
 {
-    /// <summary>
-    ///     The data for a holding that is held by two funds.
-    ///     The data comprise the holding ticker (e.g., MSFT),
-    ///     the holding name (e.g., Microsoft Corp.) and the overlap expressed
-    ///     in decimal (e.g., 0.855).
-    /// </summary>
-    public class HoldingOverlap
+  /// <summary>
+  ///     The data for a holding that is held by two funds.
+  ///     The data comprise the holding ticker (e.g., MSFT),
+  ///     the holding name (e.g., Microsoft Corp.) and the overlap expressed
+  ///     in decimal (e.g., 0.855).
+  /// </summary>
+  public class HoldingOverlap
+  {
+    public string HoldingTicker { get; set; }
+    public string HoldingName { get; set; }
+    public decimal Overlap { get; set; }
+
+    public HoldingOverlap()
     {
-        public string HoldingTicker { get; set; }
-        public string HoldingName { get; set; }
-        public decimal Overlap { get; set; }
-
-        public HoldingOverlap()
-        {
-            HoldingTicker = "";
-            HoldingName = "";
-            Overlap = 0.0M;
-        }
-
-        public HoldingOverlap(string ticker, string name, decimal overlap)
-        {
-            HoldingTicker = ticker;
-            HoldingName = name;
-            Overlap = overlap;
-        }
+      HoldingTicker = "";
+      HoldingName = "";
+      Overlap = 0.0M;
     }
+
+    public HoldingOverlap(string ticker, string name, decimal overlap)
+    {
+      HoldingTicker = ticker;
+      HoldingName = name;
+      Overlap = overlap;
+    }
+  }
 }
