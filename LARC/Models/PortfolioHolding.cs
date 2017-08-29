2e87ea0ee84ec4708ca3e3a5862edc3783434afc
Norm Clerman <norm.clerman@gmail.com>
2017-08-29 11:46:57 -0700

    Added classes PortfolioHolding and PortfolioList

---
+++ LARC/Models/PortfolioHolding.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LARC.Models
+{
+  /// <summary>
+  ///     A single holding (fund) in a portfolio.
+  /// </summary>
+  public class PortfolioHolding
+  {
+      public string Symbol { get; set; }
+      public int NumberOfShares { get; set; }
+  }
+}
---
+++ LARC/Models/PortfolioList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LARC.Models
+{
+  /// <summary>
+  ///   A list of objects of class PortfolioHolding
+  /// </summary>
+  public class PortfolioList
+  {
+    public List<PortfolioHolding> Portfolio { get; set; }
+  }
+}
