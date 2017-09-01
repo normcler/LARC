48bd06c5768e5277d39fcab3e8822f7e82fe6c81
Norm Clerman <norm.clerman@gmail.com>
2017-08-31 20:40:09 -0700

    Add SalesReport MVC (homework assignment)

---
+++ LARC/Controllers/SalesReportController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LARC.Models;
+
+namespace LARC.Controllers
+{
+  public class SalesReportController : Controller
+  {
+    // GET: SalesReport
+    public ActionResult Index(string id)
+    {
+      string connectionString = ConfigurationManager.
+           ConnectionStrings["AdventureWorks"].ConnectionString;
+
+      // The instantion of the class SalesReport that holds our
+      // data. The class is defined in the model.
+      Models.SalesReport model = new Models.SalesReport();
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        connection.Open();
+        using (SqlCommand command = connection.CreateCommand())
+        {
+          command.CommandText = "sp_GetStates";
+          command.CommandType = System.Data.CommandType.StoredProcedure;
+          using (SqlDataReader reader = command.ExecuteReader())
+          {
+            int columnPosition = reader.GetOrdinal("StateProvince");
+            while (reader.Read())
+            {
+              model.states.Add(reader.GetString(columnPosition));
+            }
+          }
+        }
+        if (!string.IsNullOrEmpty(id))
+        {
+          string stateProvince = id;
+          using (SqlCommand command = connection.CreateCommand())
+          {
+            command.CommandText = "sp_GetTopSalesByDollar";
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@stateProvince", stateProvince);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+              int amountColumn = reader.GetOrdinal("Amount");
+              int nameColumn = reader.GetOrdinal("Name");
+              int dollarAmountColumn = reader.GetOrdinal("DollarAmount");
+              while (reader.Read())
+              {
+                int amount = reader.GetInt32(amountColumn);
+                string name = reader.GetString(nameColumn);
+                decimal dollarAmount = reader.GetDecimal(dollarAmountColumn);
+                TopRevenueByDollar newItem = 
+                  new TopRevenueByDollar(amount, name, dollarAmount);
+                model.TopRevenuesByDollar.Add(newItem);
+                
+              }
+            }
+          }
+        }
+        connection.Close();
+      }
+      return View(model);
+    }
+  }
+}
---
+++ LARC/Models/SalesReport.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LARC.Models
+{
+
+  public class SalesReport
+  {
+    public List<string> states;
+    public List<TopRevenueByDollar> TopRevenuesByDollar{ get; set; }
+    public List<TopRevenueByQuantity> TopRevenuesByQuantity { get; set; }
+
+    public SalesReport()
+    {
+      this.states = new List<string>();
+      this.TopRevenuesByDollar = new List<TopRevenueByDollar>();
+      this.TopRevenuesByQuantity = new List<TopRevenueByQuantity>();
+    }
+  }
+
+
+  public class TopRevenueByDollar
+  {
+    public int Amount { get; set; }
+    public string Name { get; set; }
+    public decimal DollarAmount { get; set; }
+
+    public TopRevenueByDollar(int amount, string name, decimal dollarAmount)
+    {
+      this.Amount = amount;
+      this.Name = name;
+      this.DollarAmount = dollarAmount;
+    }
+  }
+
+  public class TopRevenueByQuantity
+  {
+    public int Amount { get; set; }
+    public string Name { get; set; }
+  }
+}
